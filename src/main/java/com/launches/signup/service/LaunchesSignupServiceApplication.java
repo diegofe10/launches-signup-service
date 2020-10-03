84350137f3e29d48238de7d952c747f8b950b2f1
Diego Gean da Fé <dafediegogean@gmail.com>
2020-10-02 22:38:21 -0300

    Initial commit

---
+++ src/main/java/com/launches/signup/service/LaunchesSignupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.launches.signup.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchesSignupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchesSignupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launches/signup/service/LaunchesSignupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launches.signup.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchesSignupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
