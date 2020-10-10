5ab57bb10a57e229e3a4dd46550345f0bb0ae4cf
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-10-10 13:14:42 -0300

    Added domain module

---
+++ launches-signup-domain/src/main/java/com/launches/signup/domain/LaunchesSignupDomainApplication.java
@@ -0,0 +1,13 @@
+package com.launches.signup.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchesSignupDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchesSignupDomainApplication.class, args);
+	}
+
+}
