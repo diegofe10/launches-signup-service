4bcb87415f34684cc8ca84941b082c2e58d8ab51
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-11-07 13:07:13 -0300

    Create LauncheController

---
+++ launches-signup-app/src/main/java/com/launches/signup/app/api/v1/controller/LauncheController.java
@@ -0,0 +1,12 @@
+package com.launches.signup.app.api.v1.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/launche")
+public class LauncheController {
+
+}
